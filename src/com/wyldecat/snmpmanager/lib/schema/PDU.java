40606eb862593408fd0ee147022a62b5b851c27a
WyldeCat <skan1543@gmail.com>
2018-06-04 21:40:32 +0900

    Add PDU interfaces

--- src/com/wyldecat/snmpmanager/lib/schema/PDU.java
+++ src/com/wyldecat/snmpmanager/lib/schema/PDU.java
@@ -18,6 +18,50 @@ public class PDU implements BERSerializable {
   private Integer32 errorIndex;
   private VarbindList varbinds;
 
+  public PDU() {
+    varbinds = new VarbindList(1);
+  }
+
+  public PDU setType(byte type) {
+    this.type.setValue(type);
+    return this;
+  }
+
+  public PDU setRequestID(int requestID, int rIDLength) {
+    this.requestID = new Integer32(requestID, rIDLength);
+    return this;
+  }
+
+  public PDU setErrorStatus(int errorStatus, int esLength) {
+    this.errorStatus = new Integer32(errorStatus, esLength);
+    return this;
+  }
+
+  public PDU setErrorIndex(int errorIndex, int eiLength) {
+    this.errorIndex = new Integer32(errorIndex, eiLength);
+    return this;
+  }
+
+  public BER.MutableByte getType() {
+    return type;
+  }
+
+  public Integer32 getRequestID() {
+    return requestID;
+  }
+
+  public Integer32 getErrorStatus() {
+    return errorStatus;
+  }
+
+  public Integer32 getErrorIndex() {
+    return errorIndex;
+  }
+
+  public Varbinds getVarbindList() {
+    return varbinds;
+  }
+
   public int getBERLength() {
     return length + 2;
   }
