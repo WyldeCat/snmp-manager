193a72a2f4f9bfa92a5461ce481c6a4466379319
WyldeCat <skan1543@gmail.com>
2018-06-04 18:01:08 +0900

    Implement methods for encoding and decoding PDU

--- src/com/wyldecat/snmpmanager/lib/schema/PDU.java
+++ src/com/wyldecat/snmpmanager/lib/schema/PDU.java
@@ -4,20 +4,42 @@ package com.wyldecat.snmpmanager.lib.schema;
 
 import java.io.OutputStream;
 import java.io.IOException;
+import org.snmp4j.asn1.BER;
 import org.snmp4j.asn1.BERInputStream;
 import org.snmp4j.asn1.BERSerializable;
 
 public class PDU implements BERSerializable {
 
+  private int length;
+
+  private BER.MutableByte type;
+  private Integer32 requestID;
+  private Integer32 errorStatus;
+  private Integer32 errorIndex;
+  private VarbindList varbinds;
+
   public int getBERLength() {
-    return 0;
+    return length + 2;
   }
 
   public int getBERPayloadLength() {
-    return 0;
+    return length;
   } 
 
-  public void decodeBER(BERInputStream bis) throws IOException { }
-  public void encodeBER(OutputStream os) throws IOException { }
+  public void decodeBER(BERInputStream bis) throws IOException {
+    length = BER.decodeHeader(bis, type);
+    requestID.decodeBER(bis);
+    errorStatus.decodeBER(bis);
+    errorIndex.decodeBER(bis);
+    varbinds.decodeBER(bis);
+  }
+
+  public void encodeBER(OutputStream os) throws IOException {
+    BER.encodeHeader(os, type, length);
+    requestId.encodeBER(os);
+    errorStatus.encodeBER(os);
+    errorIndex.encodeBER(os);
+    varbinds.encodeBER(os);
+  }
 }
 
