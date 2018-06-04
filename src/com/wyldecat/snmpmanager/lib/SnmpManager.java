89c34601e18ddba827c85e36aa7d51439ce44a44
WyldeCat <skan1543@gmail.com>
2018-06-05 01:49:48 +0900

    Implement get and test Get

--- src/com/wyldecat/snmpmanager/lib/SnmpManager.java
+++ src/com/wyldecat/snmpmanager/lib/SnmpManager.java
@@ -4,11 +4,14 @@ package com.wyldecat.snmpmanager.lib;
 
 import java.net.*;
 import java.util.Arrays;
+import java.nio.ByteBuffer;
 import android.util.Log;
 import android.widget.TextView;
 import android.os.Handler;
 
 import org.snmp4j.asn1.BER;
+import org.snmp4j.asn1.BERInputStream;
+import org.snmp4j.asn1.BEROutputStream;
 
 import com.wyldecat.snmpmanager.lib.schema.*;
 
@@ -24,7 +27,8 @@ public class SnmpManager {
   private final DatagramPacket pkt_send =
     new DatagramPacket(buff_send, buff_send.length);
 
-  private Message m;
+  private Message m_recv;
+  private Message m_send;
 
   public SnmpManager(String addr, int port) {
     DatagramSocket _sock = null;
@@ -39,6 +43,17 @@ public class SnmpManager {
 
     pkt_send.setAddress(deviceAddr);
     pkt_send.setPort(port);
+
+    m_recv = new Message();
+    m_send = new Message();
+
+    m_send.setSnmpVersion(1, 1)
+      .setCommunityString(new String("public"))
+      .setPDU(
+        new PDU().setRequestID(0x1234, 2)
+          .setErrorStatus(0, 1)
+          .setErrorIndex(0, 1)
+    );
   }
 
   private String checkOID(String OID) throws Exception {
@@ -58,7 +73,26 @@ public class SnmpManager {
   }
 
   private String get(String OID, boolean isNextRequest) throws Exception {
-    return null;
+    BEROutputStream bos = new BEROutputStream(ByteBuffer.wrap(buff_send));
+    BERInputStream bis = new BERInputStream(ByteBuffer.wrap(buff_recv));
+
+    m_send.getPDU().setType((isNextRequest ? (byte)0xa1 : (byte)0xa0))
+      .getVarbindList().addVarbind(
+        new Varbind(
+          new OID("1.3.6.1.2.1.2.2.1.7.1"),
+          new Null())
+    );
+    m_send.updateLength();
+    m_send.encodeBER(bos);
+
+    pkt_send.setLength(m_send.getBERLength());
+
+    sock.send(pkt_send);
+    sock.receive(pkt_recv);
+
+    m_recv.decodeBER(bis);
+
+    return m_recv.getPDU().getVarbindList().getVarbindAt(0).toString();
   }
 
   public String Get(String OID) throws Exception {
